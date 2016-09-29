cc0e7ab849935901657575ebd797a02675a6604b
Danny Cooper <email@dannycooper.com>
2016-09-29 10:41:18 +0100

    Add correct width for header image.

--- structure/header.php
+++ structure/header.php
@@ -112,12 +112,10 @@ if ( ! function_exists( 'zeus_custom_header_setup' ) ) {
 		add_theme_support( 'custom-header', apply_filters( 'zeus_custom_header_args', array(
 			'default-image'          => '',
 			'default-text-color'     => '000000',
-			'width'                  => 1000,
+			'width'                  => 1040,
 			'height'                 => 250,
 			'flex-height'            => true,
 			'wp-head-callback'       => 'zeus_header_style',
-			'admin-head-callback'    => 'zeus_admin_header_style',
-			'admin-preview-callback' => 'zeus_admin_header_image',
 		) ) );
 	}
 }
@@ -190,26 +188,5 @@ if ( ! function_exists( 'zeus_admin_header_style' ) ) {
 		}
 	</style>
 	<?php
-	}
-}
-
-if ( ! function_exists( 'zeus_admin_header_image' ) ) {
-	/**
-	 * Custom header image markup displayed on the Appearance > Header admin panel.
-	 *
-	 * @see zeus_custom_header_setup().
-	 */
-	function zeus_admin_header_image() {
-	?>
-	<div id="headimg">
-		<h1 class="displaying-header-text">
-			<a id="name" style="<?php echo esc_attr( 'color: #' . get_header_textcolor() ); ?>" onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-		</h1>
-		<div class="displaying-header-text" id="desc" style="<?php echo esc_attr( 'color: #' . get_header_textcolor() ); ?>"><?php bloginfo( 'description' ); ?></div>
-		<?php if ( get_header_image() ) : ?>
-		<img src="<?php header_image(); ?>" alt="">
-		<?php endif; ?>
-	</div><!-- #heading -->
-<?php
 	}
 }
